15fa6abb3f2953db6d04f918baab37aee0356238
21sukh <125358288+21sukh@users.noreply.github.com>
2023-07-27 11:19:09 +0530

    Add files via upload

---
+++ FITBEE/Modules/about.php
@@ -0,0 +1,54 @@
+<?php include 'header.php' ?>
+
+<div class="conatiner-fluid" style="background:url('asset/images/banner.jpg')";>
+			<div class="text-white text-center pt-5 pb-5" style="background-color: rgba(0,0,0,0.5); height: 100%;">	
+		    
+			<h3 class="pt-5">About Us</h3>
+			<p class="pb-5"> <a href='index.php' class="text-decoration-none text-white"> Home </a> / About</p>
+			
+			</div>	
+		</div>
+
+<div class="container pt-5 pb-5">
+	<div class="row">
+		<div class="col-md-6">
+			<img src="asset/images/luemen-rutkowski-mIHbipd5-Qw-unsplash.jpg" class="pt-3" width="500" height="550">
+		</div>
+		<div class="col-md-6">
+			<h1>Commit To Be Fit.</h1>
+<h3 class="pt-3 pb-3">Take Care Of Your Body. It's The Only Place You Have To Live In. </h3>
+<p class="text-justify">FitBee is a one-stop portal for Indians to access a wide range of health content, diet plans and Workout Plans.It is designed to empower Indians to take greater ownership of their health and wellness through the online access of better health literacy and the adoption of healthy lifestyle practices.</p>
+The website has several sections on health that address different queries you may have.Sections include:
+<div class="row pt-2">
+	<div class="col-md-6 ">
+		
+		<a href="blog.php?category=Exercise+and+Fitness" class="btn btn-block text-white" style='background:var(--themeColor2);' style="font-size:large;" role="button">Exercise and Fitness</a>
+
+		<a href="blog.php?category=Healthy+Eating" class="btn btn-block text-white" 
+		style='background:var(--themeColor2);' style="font-size:large;" role="button">Healthy Eating </a>
+	
+		<a href="blog.php?category=Weight+Loss" class="btn btn-block text-white" style='background:var(--themeColor2);' style="font-size:large;" role="button">Weight Loss</a>
+		<a href="blog.php?category=Well-being+%26+Happiness" class="btn btn-block text-white" style='background:var(--themeColor2);'style="font-size:large;" role="button"> Well-being & Happiness</a>
+	</div>
+	<div class="col-md-6 ">
+		<a href="food.php" class="btn btn-block text-white" style='background:var(--themeColor2);'style="font-size:large;" role="button">Food Nutrition Info</a>
+
+		<a href="recipe.php" class="btn btn-block text-white" style='background:var(--themeColor2);' style="font-size:large;" role="button">Recipe Search</a>
+
+		<a href="dietPlan.php" class="btn btn-block text-white" style='background:var(--themeColor2);'style="font-size:large;" role="button">Diet Plan</a>
+
+		<a href="faq.php" class="btn btn-block text-white" style='background:var(--themeColor2);'style="font-size:large;" role="button">FAQ</a>
+	</div>
+</div>
+</div>
+		<div class="col-md-12 pt-3 pb-3">
+			<p class="text-justify">
+Physical and mental health is closely linked, so that a change (good or bad) in one directly affects the other. Consequently, some of the tips will include suggestions for emotional and mental "healthy living. Wellness is about more than just physical health. Wellness as the active pursuit of activities, choices and lifestyles that lead to a state of holistic health.You can find the information you need to reach your nutrition and fitness goals. We dedicated to empowering users with credible and up-to-date advice on Mental Health, diet, weight loss, and exercise topics.
+</p>
+		</div>
+	</div>
+</div>		
+<?php include 'footer.php' ?>
+
+
+
